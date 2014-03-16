bb7550ec67900f2ca49e02237b007d7520671265
Andrew <aj.cumine@btinternet.com>
2014-03-16 13:50:09 +0000

    new my_inspect test implemetation for default argument = nil

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,7 +1,7 @@
 class Array
 
-  def my_inject(arg, &block)
-    array = ([arg] << self).flatten
+  def my_inject(arg = nil, &block)
+    array = ([arg] << self).flatten.compact
     result = array[0]
     array.each {|n| result = yield(result, n)}
     result
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -11,5 +11,8 @@ describe Array do
     it "should sum the array" do
       expect([1,2,3,4].my_inject(0){|memo, n| memo + n}).to eq(10)
     end
+    it "should multiply the array values" do
+      expect([1,2,3,4].my_inject{|memo, n| memo * n}).to eq(24)
+    end
   end
 end
