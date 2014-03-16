c711cfaa0fb591b56c621a5bddbde2fc51fa7b77
Andrew <aj.cumine@btinternet.com>
2014-03-16 13:41:14 +0000

    implement my_inject method

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,3 +1,10 @@
 class Array
 
+  def my_inject(arg, &block)
+    array = ([arg] << self).flatten
+    result = array[0]
+    array.each {|n| result = yield(result, n)}
+    result
+  end
+
 end
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -6,4 +6,10 @@ describe Array do
       expect([1,2,3,4].inject(0){|memo, n| memo + n}).to eq(10)
     end
   end
+
+  context "my_inject" do
+    it "should sum the array" do
+      expect([1,2,3,4].my_inject(0){|memo, n| memo + n}).to eq(10)
+    end
+  end
 end
