77fb0ae74560069afff22aff089416e8356af661
Andrew <aj.cumine@btinternet.com>
2014-03-17 09:47:55 +0000

    allows nil as array input

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,7 +1,7 @@
 class Array
 
   def my_inject(arg = nil, &block)
-    array = ([arg] << self).flatten.compact
+    array = ([arg].compact << self).flatten
     result = array[0]
     array.each {|n| result = yield(result, n)}
     result
