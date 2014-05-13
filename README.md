6e9f1cf9a2bc81b8bce8a54299022c19785a93bf
Andrew <ajcumine@gmail.com>
2014-05-13 14:14:07 +0100

    add readme

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Inject and Gem Test
 
-### Week 4 Test @ MakersAcademy
+A test set in Week 4 at [Makers Academy](http://www.makersacademy.com). 
+
+The test was set to test the understanding of Ruby and TDD with the inclusion of gems.
+
+
+The test included:
+
+* recreating the inject method
+* creating small app including the twilio gem
 
--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -20,25 +20,27 @@ class Takeaway
     result
   end
 
+
+  def check_send_message
+    check_total ? send_message(message) : (raise "Totals do not match")
+  end
+
   def check_total
     return true if total == order.total
     false
   end
 
+  def send_message(message)
+    @client.account.messages.create({:from => "+441296340122", :to => "+447789172199", :body => message})
+  end
+  
   def message
     time = Time.now + (60*60)
     "Thank you! Your order was placed successfully and that it will be delivered before #{time.strftime("%H%M")}"
-  end
-
-  def check_send_message
-    check_total ? send_message(message) : (raise "Totals do not match")
   end
 
   def client
     @client = Twilio::REST::Client.new('ACfee5d6e934b3442fa4d6a39eaa0a8b5f', 'a4a94a4ca87546cc9e60cf79b03abba1')
   end
 
-  def send_message(message)
-    @client.account.messages.create({:from => "+441296340122", :to => "+447789172199", :body => message})
-  end
 end
